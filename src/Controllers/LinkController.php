ccdac07118eb92e88c5012728909a616075e8ee9
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-26 22:07:08 -0300

    chore(controllers): Refactor LinkController for better code structure
    - Reorganized LinkController code into more readable and structured blocks.
    - Separated create method functionality to improve code maintainability.
    - Added input validation and error handling logic to the create method.
    - Implemented the generateShortLink and generateIdentifier methods for cleaner code.
    - Updated the generateIdentifier method to avoid generating repeated characters.

--- src/Controllers/LinkController.php
+++ src/Controllers/LinkController.php
@@ -2,17 +2,63 @@
 
 namespace AdaiasMagdiel\Sputnik\Controllers;
 
-class LinkController extends Controller {
+class LinkController extends Controller
+{
 	public function __construct()
 	{
 		parent::__construct();
 	}
 
-	public function index() {
+	public function index()
+	{
 		$this->renderTemplate("index");
 	}
 
-	public function create() {
-		$this->renderTemplate("index");
+	public function create()
+	{
+		$errors = [];
+		$result = "";
+		$linkRaw = "";
+
+		$link = $_POST['link'] ?? "";
+		if (!filter_var($link, FILTER_VALIDATE_URL)) {
+			$errors = ["This value should be a valid URL."];
+			$linkRaw = $link;
+		}
+
+		$result = $this->generateShortLink($link);
+
+		$this->renderTemplate("index", [
+			"errors" => $errors,
+			"result" => $result,
+			"link" => $linkRaw
+		]);
+	}
+
+	public function generateShortLink(string $link)
+	{
+		$id = $this->generateIdentifier();
+		return $id;
+	}
+
+	public function generateIdentifier(int $limit = 7)
+	{
+		$chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-_.';
+		$rand_str = '';
+		$max = mb_strlen($chars) - 1;
+
+		for ($i = 0; $i < $limit; $i++) {
+			$rand_index = random_int(0, $max);
+			$char = $chars[$rand_index];
+
+			while ($char === substr($rand_str, -1)) {
+				$rand_index = random_int(0, $max);
+				$char = $chars[$rand_index];
+			}
+
+			$rand_str .= $char;
+		}
+
+		return $rand_str;
 	}
 }
