314649c48853eff8aa53275622fb1fd0638d27ad
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-27 22:20:29 -0300

    refactor(routes): Refactor route handling for better readability
    The route handling in routes.php has been refactored to improve readability
    and consistency. Changes include:
    - Added whitespace between the 'fn' keyword and parameters for better clarity.
    - Added a new route '/[id]' with a callback function to retrieve a specific
      link using the provided ID.
    - These changes aim to make the code easier to understand and maintain.

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,8 @@ $controller = new LinkController();
 
 Router::initialize();
 
-Router::get('/', fn() => $controller->index());
-Router::post('/', fn() => $controller->create());
+Router::get('/', fn () => $controller->index());
+Router::post('/', fn () => $controller->create());
+Router::get('/[id]', fn (string $id) => $controller->get($id));
 
 Router::execute();
