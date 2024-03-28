43ef850cafd092297972def3541d564718e55942
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-27 22:19:28 -0300

    feat(src): Add configuration settings
    This commit introduces a new file, config.php, that contains a Config class
    with static properties for DSN, user, and password settings. These values
    are essential for database connectivity and other configurations within the
    application.

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AdaiasMagdiel\Sputnik;
+
+
+class Config
+{
+	public static string $dsn = "sqlite:" . __DIR__ . "/Database/db.sqlite";
+	public static string $user = "";
+	public static string $password = "";
+}
