5e90d9f61278db33f92475bf34fc1e34cbbe48c1
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-27 22:19:14 -0300

    feat(repos): Add LinkRepository and Repository classes
    This commit introduces the LinkRepository and Repository classes.
    
    - LinkRepository is responsible for managing operations related to links,
      such as saving short links and retrieving links by identifier.
    - Repository provides a base class for database repositories with methods
      for creating tables based on defined fields.
    
    These classes enhance the functionality and structure of the project by
    separating concerns and promoting reusability of database operations.

---
+++ src/Repositories/LinkRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AdaiasMagdiel\Sputnik\Repositories;
+
+class LinkRepository extends Repository
+{
+	protected string $table = "links";
+	protected array $fields = [
+		"id" => "INTEGER PRIMARY KEY AUTOINCREMENT",
+		"link" => "VARCHAR(255) NOT NULL UNIQUE",
+		"identifier" => "VARCHAR(25) NOT NULL UNIQUE"
+	];
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->createTables();
+	}
+
+	public function saveShortLink(string $link, string $identifier)
+	{
+		$res = $this->getIdentifierByLink($link);
+		if ($res) return $res->identifier;
+
+		$sql = "INSERT INTO links(link, identifier) VALUES(:link, :identifier);";
+		$stmt = $this->conn->prepare($sql);
+
+		$stmt->execute(["link" => $link, "identifier" => $identifier]);
+		return $identifier;
+	}
+
+	public function getLinkByIdentifier(string $identifier)
+	{
+		$sql = "SELECT link FROM links WHERE identifier = :identifier;";
+		$stmt = $this->conn->prepare($sql);
+
+		$stmt->execute(["identifier" => $identifier]);
+		return $stmt->fetch();
+	}
+
+	public function getIdentifierByLink(string $link)
+	{
+		$sql = "SELECT identifier FROM links WHERE link = :link;";
+		$stmt = $this->conn->prepare($sql);
+
+		$stmt->execute(["link" => $link]);
+		return $stmt->fetch();
+	}
+}
---
+++ src/Repositories/Repository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AdaiasMagdiel\Sputnik\Repositories;
+
+use PDO;
+use AdaiasMagdiel\Sputnik\Database\Connection;
+
+class Repository
+{
+	protected PDO $conn;
+	protected string $table;
+	protected array $fields;
+
+	public function __construct()
+	{
+		$this->conn = Connection::getConnection();
+	}
+
+	protected function createTables()
+	{
+		$values = [];
+		foreach ($this->fields as $key => $value) {
+			$values[] = "{$key} {$value}";
+		}
+		$values = join(", ", $values);
+
+		$sql = "CREATE TABLE IF NOT EXISTS {$this->table}({$values});";
+		return $this->conn->exec($sql);
+	}
+}
